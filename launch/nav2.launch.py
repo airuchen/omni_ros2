b95acaecf5f690ef76350cf1ff4613a725b42587
airuchen <b10307115@gapps.ntust.edu.tw>
2019-11-25 14:58:38 +0800

    Edited navigation launch to open original nav2_bringup

--- launch/nav2.launch.py
+++ launch/nav2.launch.py
@@ -42,12 +42,12 @@ def generate_launch_description():
             'config',
             'nav2.yaml'))
 
-    nav2_launch_file_dir = os.path.join(get_package_share_directory('omni_ros2'), 'launch')
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
 
     rviz_config_dir = os.path.join(
-        get_package_share_directory('omni_ros2'),
-        'config',
-        'default.rviz')
+        get_package_share_directory('nav2_bringup'),
+        'launch',
+        'nav2_default_view.rviz')
 
     return LaunchDescription([      
 
@@ -68,7 +68,11 @@ def generate_launch_description():
             description='Use simulation (Gazebo) clock if true'),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/nav2_bringup.launch.py']),
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/nav2_bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params': param_dir}.items(),
         ),
 
         # SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1'),
@@ -76,7 +80,7 @@ def generate_launch_description():
         Node(
             package='rviz2',
             node_executable='rviz2',
-            # node_name='rviz2',
+            node_name='rviz2',
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
             output='log'),
